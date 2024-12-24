0c6809becc7cb8713ad850fb8484338f901c4339
Flavien Electronics <australianmagpie@gmail.com>
2024-12-24 08:33:19 +0100

    Nice droite

--- MUL4b/Print/plot_csv.py
+++ MUL4b/Print/plot_csv.py
@@ -3,29 +3,26 @@ import matplotlib.pyplot as plt
 
 dataList = pd.read_csv("MULTIPLIEUR_4_BITS.csv")
 
-print(dataList.info())
-print(dataList.head())
-
 time = dataList["Time(s)"]
 
-P = [dataList["P0(V)"],
-    dataList["P1(V)"]+1,
-    dataList["P2(V)"]+2,
-    dataList["P3(V)"]+3,
-    dataList["P4(V)"]+4,
-    dataList["P5(V)"]+5,
-    dataList["P6(V)"]+6,
-    dataList["P7(V)"]+7]
+A = [dataList["A0(V)"]+0,
+    dataList["A1(V)"]+1,
+    dataList["A2(V)"]+2,
+    dataList["A3(V)"]+3]
 
-A = [dataList["A0(V)"]+8,
-    dataList["A1(V)"]+9,
-    dataList["A2(V)"]+10,
-    dataList["A3(V)"]+11]
+B = [dataList["B0(V)"]+5,
+    dataList["B1(V)"]+6,
+    dataList["B2(V)"]+7,
+    dataList["B3(V)"]+8]
 
-B = [dataList["B0(V)"]+12,
-    dataList["B1(V)"]+13,
-    dataList["B2(V)"]+14,
-    dataList["B3(V)"]+15]
+P = [dataList["P0(V)"]+10,
+    dataList["P1(V)"]+11,
+    dataList["P2(V)"]+12,
+    dataList["P3(V)"]+13,
+    dataList["P4(V)"]+14,
+    dataList["P5(V)"]+15,
+    dataList["P6(V)"]+16,
+    dataList["P7(V)"]+17]
 
 for i in range(0,len(P)):
     plt.plot(time,P[i],label ="P"+str(i))
@@ -36,7 +33,19 @@ for i in range(0,len(A)):
 for i in range(0,len(B)):
     plt.plot(time,B[i],label ="B"+str(i))
 
+t_start = 0
+t_stop = -1
+while (i != len(time)):
+    if A[0][i] > 0.4 and t_start == 0:
+        t_start = i
+        t_stop = 0
+    
+    if A[0][i] < 0.2 and t_stop == 0:
+        t_stop = i
+        middle = int((t_stop - t_start) / 2)
+        plt.vlines(x=time[t_start+middle], ymin=0, ymax=18, color='k', linestyle='-', linewidth=1)
+        t_start = 0
+    i+=1
     
 plt.show()
-
 
